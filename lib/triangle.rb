5278ff9ce3161c231cea1575e12ac9b52064514c
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-06-01 23:01:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,10 +3,12 @@ class Triangle
   def initialize(length1, length2, length3)
     @length1 = length1
     @length2 = length2
-    @length3 = length3 
-    
+    @length3 = length3
+  end
+
+  def kind 
     
   end
-  
+
 
 end
