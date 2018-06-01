844d96d0efa6c62084723f8deec090f92421cc32
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-06-01 23:27:08 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,7 +16,7 @@ class Triangle
       array.all? do |x| x == array[1] || x == array[2] || x == array[3]
     :isosceles
   elsif
-    @length1 != @length2 && @length1 != @length3 && @length2 =! @length3
+    array.all? 
     :scalene
   end
   end
