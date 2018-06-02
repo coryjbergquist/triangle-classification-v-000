2bf79f901f814c60c81ca6e5399e7af4f4c40713
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-06-02 00:11:24 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,6 +13,8 @@ class Triangle
   if
   @length1 <= 0 || @length2 <= 0 || @length3 <= 0
         raise TriangleError
+  elsif
+    (@length1 + @length2) < @length3 || (@length1 + @length3) < @length2 || (@length3 + @length2) < @length1
    elsif
      array.all? do |x| x == array[1] end
      :equilateral
