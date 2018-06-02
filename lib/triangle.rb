dd1ebea27a350eefcd3a249e17fc41caea70b5be
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-06-02 00:12:26 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
   @length1 <= 0 || @length2 <= 0 || @length3 <= 0
         raise TriangleError
   elsif
-    (@length1 + @length2) < @length3 || (@length1 + @length3) < @length2 || (@length3 + @length2) < @length1
+    (@length1 + @length2) <= @length3 || (@length1 + @length3) <= @length2 || (@length3 + @length2) <= @length1
     raise TriangleError
    elsif
      array.all? do |x| x == array[1] end
